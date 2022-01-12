788b04acdd4b504f608b608909d7f137298ca422
kleinbub <johann.kleinbub@gmail.com>
2022-01-12 17:10:41 +0100

    new functions tss tse. The eternal struggle with the ts object

--- R/fixing_ts.R
+++ R/fixing_ts.R
@@ -13,10 +13,16 @@ scale.ts = function(x, center = TRUE, scale = TRUE){
 #'
 #' @param x a ts object
 #' @export
-tss = function(x){tsp(x)[1]}
+tss = function(x){return(tsp(x)[1L])}
 #' Time Series End
 #' this returns the end of a ts object in time units
 #'
 #' @param x a ts object
 #' @export
-tse = function(x){tsp(x)[3]}
+tse = function(x){return(tsp(x)[2L])}
+
+#' Duration of a ts object in time units
+#' @param x a ts object
+#'
+#' @export
+duration = function(x){return(tse(x)-tss(x))}
